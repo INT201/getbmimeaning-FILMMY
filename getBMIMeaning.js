a4672532e480231735aca559e2ddf2c06b20bc8c
FILMMY <pratipran.s@mail.kmutt.ac.th>
2022-09-06 08:34:09 +0700

    Update 1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,16 @@
 const { template } = require('@babel/core')
-
-function calculateBMI(weight, height) {
-  //code here
+function calculateBMI (weight, height) {
+  return weight/(height*height)
 }
-function getBMIMeaning(weight, height) {
-  //code here
-}
+function getBMIMeaning(weight, height){
+  let BMI = calculateBMI() 
+  if (BMI < 18.5){
+      return "UnderWeight"
+  }else if (BMI < 25) {
+      return "Normal Weight"
+  }else (BMI>25) 
+      return "Overweight"
+     
+} 
+console.log(getBMIMeaning(60,1.73))
 module.exports = getBMIMeaning
